befd29956cb3e19d833ef4aa22bf7f2ba5d94219
unknown <yanadsp23@gmail.com>
2023-04-06 11:38:21 -0300

    first project

---
+++ cards/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	//var card string = "Ace of Spades"
+	cards := deck{"Ace of Diamonds"} // the : is only used in the first declaration of the variable
+	cards.print()
+}
