3f561548480ab206ebbbf7e3e5c483aa3d504a87
unknown <yanadsp23@gmail.com>
2023-04-12 16:52:10 -0300

    go

--- cards/main.go
+++ cards/main.go
@@ -2,9 +2,6 @@ package main
 
 func main() {
 	//var card string = "Ace of Spades"
-	cards := newDeck() // the : is only used in the first declaration of the variable
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
